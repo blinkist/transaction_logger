e2569e850d946bf335b7c4fc0272eaef8d27c6f6
John Donner <johnbdonner@gmail.com>
2015-07-21 16:19:16 +0200

    Addressed #26 and hid module methods Wrapped a lot of the methods into two new classes: Helper and Configure. Also updated the README.md and version.

--- lib/transaction_logger/version.rb
+++ lib/transaction_logger/version.rb
@@ -1,3 +1,3 @@
 module TransactionLogger
-  VERSION = "1.0.0"
+  VERSION = "1.0.1"
 end
--- spec/transaction_logger_spec.rb
+++ spec/transaction_logger_spec.rb
@@ -46,31 +46,34 @@ describe TransactionLogger do
     }
   end
 
-  describe ".log_prefix" do
-    context "when there is no prefix" do
-      it "does not change the output" do
-        expect(subject.to_hash).to include("name" => "undefined")
-      end
-    end
+  describe TransactionLogger::Configure do
 
-    context "when a prefix is defined" do
-      let (:prefix) { "bta_" }
-
-      before :example do
-        described_class.log_prefix = prefix
+    describe ".log_prefix" do
+      context "when there is no prefix" do
+        it "does not change the output" do
+          expect(subject.to_hash).to include("name" => "undefined")
+        end
       end
 
-      subject {
-        TransactionLogger::Transaction.new(
-          { prefix: described_class.log_prefix, logger: Logger.new(STDOUT), level_threshold: nil }, test_lmbda)
-      }
+      context "when a prefix is defined" do
+        let (:prefix) { "bta_" }
 
-      after :example do
-        described_class.log_prefix = ""
-      end
+        before :example do
+          described_class.log_prefix = prefix
+        end
 
-      it "adds the prefix to every key" do
-        expect(subject.to_hash).to include("bta_name" => "undefined")
+        subject {
+          TransactionLogger::Transaction.new(
+            { prefix: described_class.log_prefix, logger: Logger.new(STDOUT), level_threshold: nil }, test_lmbda)
+        }
+
+        after :example do
+          described_class.log_prefix = ""
+        end
+
+        it "adds the prefix to every key" do
+          expect(subject.to_hash).to include("bta_name" => "undefined")
+        end
       end
     end
   end
